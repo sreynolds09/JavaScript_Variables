47289e5fb94bf59f62671475d0b6e7d89411752b
sreynolds09 <sara.reynolds.clan@gmail.com>
2025-07-04 16:36:58 -0400

    initial commit

--- app.js
+++ app.js
@@ -3,8 +3,55 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+// Exercise 1: Declaring Variables
+const firstName = "Sara"; // Assigned value directly as required in Exercise 2
+let lastName;         // Will assign value in Exercise 2
+var age;              // Will assign value in Exercise 2
 
-// Exercise 2
+// Exercise 2: Assigning Values to Variables
+// Assign your actual first name, last name, and age here
+lastName = "Reynolds";
+age = 30;
+// Removed unnecessary firstNameUpdated and redeclaration
 
-// Exercise 3
+// Exercise 3: Declaring and Assigning Values to Variables
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4: Declaring and Assigning Values to Variables x2
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Exercise 5: Declaring and Assigning Values to Variables x3
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+
+
+// Log variables to the console
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(language);
+console.log(createdYear);
+console.log(isCaseSensitive);
+console.log(price);
+console.log(isOnSale);
+console.log(salePercentage);
+console.log(stock);
+console.log(inStock);
+console.log(selectedSize);
+console.log(title);
+console.log(author);
+console.log(pageCount);
+console.log(bookmark);
+console.log(hasRead);
+
